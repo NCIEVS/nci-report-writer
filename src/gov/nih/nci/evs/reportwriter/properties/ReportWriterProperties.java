5a4f9aec68ccfca0c2e41ac0eeadd0c9cdbe11f6
David Yee <yeed>
2009-08-28 16:45:40 +0000

    GF23072: Reformatted ReportWriterProperties.
    SVN-Revision: 261

--- src/gov/nih/nci/evs/reportwriter/properties/ReportWriterProperties.java
+++ src/gov/nih/nci/evs/reportwriter/properties/ReportWriterProperties.java
@@ -1,81 +1,89 @@
 package gov.nih.nci.evs.reportwriter.properties;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.util.Iterator;
-import java.util.Properties;
+import java.io.*;
+import java.util.*;
 
-import org.apache.log4j.Logger;
-
+import org.apache.log4j.*;
 
 /**
- * Singleton for accessing Report Writer Properties.
- *
- * @author <a href="mailto:rajasimhah@mail.nih.gov">Harsha Rajasimha</a>
- *
+ * <!-- LICENSE_TEXT_START --> Copyright 2008,2009 NGIT. This software was
+ * developed in conjunction with the National Cancer Institute, and so to the
+ * extent government employees are co-authors, any rights in such works shall be
+ * subject to Title 17 of the United States Code, section 105. Redistribution
+ * and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met: 1. Redistributions
+ * of source code must retain the above copyright notice, this list of
+ * conditions and the disclaimer of Article 3, below. Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. 2. The end-user documentation included with the
+ * redistribution, if any, must include the following acknowledgment: "This
+ * product includes software developed by NGIT and the National Cancer
+ * Institute." If no such end-user documentation is to be included, this
+ * acknowledgment shall appear in the software itself, wherever such third-party
+ * acknowledgments normally appear. 3. The names
+ * "The National Cancer Institute", "NCI" and "NGIT" must not be used to endorse
+ * or promote products derived from this software. 4. This license does not
+ * authorize the incorporation of this software into any third party proprietary
+ * programs. This license does not authorize the recipient to use any trademarks
+ * owned by either NCI or NGIT 5. THIS SOFTWARE IS PROVIDED "AS IS," AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE) ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE NATIONAL CANCER INSTITUTE, NGIT, OR THEIR
+ * AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. <!-- LICENSE_TEXT_END -->
  */
 public class ReportWriterProperties {
+    public static final String EVS_SERVICE_URL = "EVS_SERVICE_URL";
+    public static final String REPORT_DOWNLOAD_DIRECTORY = "REPORT_DOWNLOAD_DIRECTORY";
+    public static final String MAXIMUM_LEVEL = "MAXIMUM_LEVEL";
+    public static final String MAXIMUM_RETURN = "MAXIMUM_RETURN";
 
-       //KLO
-		public static final String EVS_SERVICE_URL = "EVS_SERVICE_URL";
-		public static final String REPORT_DOWNLOAD_DIRECTORY = "REPORT_DOWNLOAD_DIRECTORY";
-		public static final String MAXIMUM_LEVEL = "MAXIMUM_LEVEL";
-		public static final String MAXIMUM_RETURN = "MAXIMUM_RETURN";
+    private static Logger logger = Logger
+        .getLogger(ReportWriterProperties.class);
+    private static ReportWriterProperties reportwriterProperties;
+    private static Properties properties = new Properties();
 
-	    private static Logger logger = Logger.getLogger(ReportWriterProperties.class);
+    private ReportWriterProperties() {
+    }
 
-		private static ReportWriterProperties reportwriterProperties;
+    public static ReportWriterProperties getInstance() throws Exception {
+        if (reportwriterProperties == null) {
+            synchronized (ReportWriterProperties.class) {
+                if (reportwriterProperties == null) {
+                    reportwriterProperties = new ReportWriterProperties();
+                    loadProperties();
+                }
+            }
+        }
+        return reportwriterProperties;
+    }
 
-	    private static Properties properties = new Properties();
+    public static String getProperty(String key) throws Exception {
+        return properties.getProperty(key);
+    }
 
+    private static void loadProperties() throws Exception {
+        String propertyFile = System
+            .getProperty("gov.nih.nci.cacore.ncireportwriterProperties");
+        logger.info("File location= " + propertyFile);
 
-	    /**
-	     * Private constructor for singleton pattern.
-	     */
-		private ReportWriterProperties() {}
+        if (propertyFile != null && propertyFile.length() > 0) {
+            FileInputStream fis = new FileInputStream(new File(propertyFile));
+            properties.load(fis);
+        } else
+            System.out.println("propertyFile is null");
 
-		/**
-		 * Gets the single instance of ReportWriterProperties.
-		 *
-		 * @return single instance of ReportWriterProperties
-		 *
-		 * @throws Exception the exception
-		 */
-		public static ReportWriterProperties getInstance() throws Exception{
-			if(reportwriterProperties == null) {
-				synchronized(ReportWriterProperties.class) {
-					if(reportwriterProperties == null) {
-						reportwriterProperties = new ReportWriterProperties();
-						loadProperties();
-					}
-				}
-			}
-			return reportwriterProperties ;
-		}
-
-
-	    //public String getProperty(String key) throws Exception{
-		public static String getProperty(String key) throws Exception{
-	    	return properties.getProperty(key);
-	    }
-
-
-	    private static void loadProperties() throws Exception{
-			String propertyFile = System.getProperty("gov.nih.nci.cacore.ncireportwriterProperties");
-
-			logger.info("File location= "+ propertyFile);
-
-			if(propertyFile != null && propertyFile.length() > 0){
-				FileInputStream fis = new FileInputStream(new File(propertyFile));
-				properties.load(fis);
-			}
-			else System.out.println("propertyFile is null");
-
-			logger.debug("List of properties:");
-			for(Iterator i = properties.keySet().iterator(); i.hasNext();){
-				String key = (String)i.next();
-				String value  = properties.getProperty(key);
-	            logger.debug("* " + key + ": " + value);
-			}
-		}
-	}
+        logger.debug("List of properties:");
+        for (Iterator<?> i = properties.keySet().iterator(); i.hasNext();) {
+            String key = (String) i.next();
+            String value = properties.getProperty(key);
+            logger.debug("* " + key + ": " + value);
+        }
+    }
+}
