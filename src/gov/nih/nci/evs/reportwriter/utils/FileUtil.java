b74b7983482b3c05dabd7ddcddea9428efd03033
David Yee <yeed>
2009-08-28 16:41:11 +0000

    GF23072: Cleaning compilation messages and formatted copyright info.
    SVN-Revision: 260

--- src/gov/nih/nci/evs/reportwriter/utils/FileUtil.java
+++ src/gov/nih/nci/evs/reportwriter/utils/FileUtil.java
@@ -2,43 +2,52 @@ package gov.nih.nci.evs.reportwriter.utils;
 
 import java.io.*;
 import java.util.*;
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import org.apache.poi.poifs.filesystem.DirectoryEntry;
-import org.apache.poi.poifs.filesystem.DocumentEntry;
 
 import org.apache.poi.hssf.usermodel.*;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.hssf.util.*;
 
 
 /**
-  * <!-- LICENSE_TEXT_START -->
-* Copyright 2008,2009 NGIT. This software was developed in conjunction with the National Cancer Institute,
-* and so to the extent government employees are co-authors, any rights in such works shall be subject to Title 17 of the United States Code, section 105.
-* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-* 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the disclaimer of Article 3, below. Redistributions
-* in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other
-* materials provided with the distribution.
-* 2. The end-user documentation included with the redistribution, if any, must include the following acknowledgment:
-* "This product includes software developed by NGIT and the National Cancer Institute."
-* If no such end-user documentation is to be included, this acknowledgment shall appear in the software itself,
-* wherever such third-party acknowledgments normally appear.
-* 3. The names "The National Cancer Institute", "NCI" and "NGIT" must not be used to endorse or promote products derived from this software.
-* 4. This license does not authorize the incorporation of this software into any third party proprietary programs. This license does not authorize
-* the recipient to use any trademarks owned by either NCI or NGIT
-* 5. THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED WARRANTIES, (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE) ARE DISCLAIMED. IN NO EVENT SHALL THE NATIONAL CANCER INSTITUTE,
-* NGIT, OR THEIR AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  * <!-- LICENSE_TEXT_END -->
-  */
+ * <!-- LICENSE_TEXT_START -->
+ * Copyright 2008,2009 NGIT. This software was developed in conjunction 
+ * with the National Cancer Institute, and so to the extent government 
+ * employees are co-authors, any rights in such works shall be subject 
+ * to Title 17 of the United States Code, section 105.
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions 
+ * are met:
+ *   1. Redistributions of source code must retain the above copyright 
+ *      notice, this list of conditions and the disclaimer of Article 3, 
+ *      below. Redistributions in binary form must reproduce the above 
+ *      copyright notice, this list of conditions and the following 
+ *      disclaimer in the documentation and/or other materials provided 
+ *      with the distribution.
+ *   2. The end-user documentation included with the redistribution, 
+ *      if any, must include the following acknowledgment:
+ *      "This product includes software developed by NGIT and the National 
+ *      Cancer Institute."   If no such end-user documentation is to be
+ *      included, this acknowledgment shall appear in the software itself,
+ *      wherever such third-party acknowledgments normally appear.
+ *   3. The names "The National Cancer Institute", "NCI" and "NGIT" must 
+ *      not be used to endorse or promote products derived from this software.
+ *   4. This license does not authorize the incorporation of this software
+ *      into any third party proprietary programs. This license does not 
+ *      authorize the recipient to use any trademarks owned by either NCI 
+ *      or NGIT 
+ *   5. THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED 
+ *      WARRANTIES, (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+ *      OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE) ARE 
+ *      DISCLAIMED. IN NO EVENT SHALL THE NATIONAL CANCER INSTITUTE,
+ *      NGIT, OR THEIR AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ *      INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ *      BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ *      LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ *      CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+ *      LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ *      ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ *      POSSIBILITY OF SUCH DAMAGE.
+ * <!-- LICENSE_TEXT_END -->
+ */
 
 /**
   * @author EVS Team
@@ -57,7 +66,7 @@ public class FileUtil {
 
     private static Vector<String> parseData(String line, String tab)
     {
-		Vector data_vec = new Vector();
+		Vector<String> data_vec = new Vector<String>();
 	    StringTokenizer st = new StringTokenizer(line, tab, true);  //RWW GF#20743, delimiters are returned as tokens
 	    boolean lastWasDelim = true;                                // first value could be a tab
 		while (st.hasMoreTokens()) {
@@ -87,7 +96,7 @@ public class FileUtil {
 
 		Boolean[] a = null;
 
-		String heading = null;
+		//String heading = null;
 
 		try {
 		   fis = new FileInputStream(file);
@@ -103,7 +112,7 @@ public class FileUtil {
 				  Vector<String> v = parseData(line, delimiter);
 				  if (rownum == 0)
 				  {
-					  heading = line;
+					  //heading = line;
 					  a = new Boolean[v.size()];
 					  for (int i=0; i<v.size(); i++) {
 						  a[i] = Boolean.FALSE;
@@ -238,9 +247,8 @@ public class FileUtil {
 
    public static void main(String[] args) {
       try {
-         FileUtil test = new FileUtil();
    	     String delimiter = "\t";
-		 test.convertToExcel(args[0], delimiter, args[1]);
+   	     FileUtil.convertToExcel(args[0], delimiter, args[1]);
 
       } catch (Exception e) {
          e.printStackTrace();
