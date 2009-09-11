705e051919935ba283e00662ae171c82c2b1245a
David Yee <yeed>
2009-09-11 15:30:54 +0000

    GF23129: Cleaned up LoginFilter class.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncirw/trunk@346 e1e6a9b6-88fc-49c9-b504-51d68f5d0411

--- software/ncireportwriter/src/java/gov/nih/nci/evs/reportwriter/bean/LoginFilter.java
+++ software/ncireportwriter/src/java/gov/nih/nci/evs/reportwriter/bean/LoginFilter.java
@@ -1,19 +1,19 @@
 package gov.nih.nci.evs.reportwriter.bean;
 
-import java.io.IOException;
+import java.io.*;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import org.apache.log4j.*;
+
 /**
  * @author EVS Team
  * @version 1.0
- * 
- *          Modification history Initial implementation rajasimhah@mail.nih.gov
- * 
  */
 
 public class LoginFilter implements Filter {
+    private static Logger logger = Logger.getLogger(LoginFilter.class);
     private FilterConfig filterConfig = null;
 
     public void destroy() {
@@ -25,64 +25,47 @@ public class LoginFilter implements Filter {
             IllegalStateException {
 
         try {
-            HttpServletRequest hsr = (HttpServletRequest) request;
-            HttpServletResponse hsr2 = (HttpServletResponse) response;
+            HttpServletRequest requestL = (HttpServletRequest) request;
+            HttpServletResponse responseL = (HttpServletResponse) response;
 
-            String contxt = hsr.getContextPath();
-            String uri = hsr.getRequestURI();
+            String contxt = requestL.getContextPath();
+            String uri = requestL.getRequestURI();
             int index = uri.lastIndexOf("/");
             String path = uri.substring(index);
-            String queryString = hsr.getQueryString();
-            String page = contxt;
 
             if (path.equals("/login.jsf") || path.equals("/failure.jsf")
                 || path.equals("/") || path.equals("/download_nologin.jsf")
                 || path.equals("/download.jsf")) {
                 chain.doFilter(request, response);
-            } else {
-                try {
-                    HttpSession session = hsr.getSession();
+                return;
+            }
 
-                    Boolean svalid = null;
-                    if (session != null) {
-                        if (path.equals("/logout.jsf")) {
-                            try {
-                                session.invalidate();
-                                queryString = "logout";
-                                hsr2.sendRedirect(page
-                                    + (queryString == null ? "" : "?"
-                                        + queryString));
-                            } catch (Exception e) {
-                                // e.printStackTrace();
-                            }
-                        }
-                        try {
-                            svalid =
-                                (Boolean) session
-                                    .getAttribute("isSessionValid");
-                        } catch (Exception e) {
-                            // e.printStackTrace();
-                        }
-                        if (svalid == null || svalid.equals(Boolean.FALSE)) {
-                            hsr2
-                                .sendRedirect(page
-                                    + (queryString == null ? "" : "?"
-                                        + queryString));
-                        } else {
-                            try {
-                                chain.doFilter(request, response);
-                            } catch (Exception e) {
-                                // e.printStackTrace();
-                            }
-                        }
-                    }
-                } catch (Exception e) {
-                    // e.printStackTrace();
-                }
+            HttpSession session = requestL.getSession();
+            if (session == null)
+                return;
+
+            String queryString = requestL.getQueryString();
+            String page = contxt;
+            if (path.equals("/logout.jsf")) {
+                session.invalidate();
+                queryString = "logout";
+                responseL.sendRedirect(redirectPage(page, queryString));
+                return;
+            }
+
+            Boolean svalid = (Boolean) session.getAttribute("isSessionValid");
+            if (svalid == null || svalid.equals(Boolean.FALSE)) {
+                responseL.sendRedirect(redirectPage(page, queryString));
+            } else {
+                chain.doFilter(request, response);
             }
         } catch (Exception e) {
-            // e.printStackTrace();
+            logger.error(e.getClass().getSimpleName() + ": " + e.getMessage());
         }
+    }
+    
+    private String redirectPage(String page, String queryString) {
+        return page + (queryString == null ? "" : "?" + queryString);
     }
 
     public void init(FilterConfig filterConfig) throws ServletException {
