b84aa13601691452cb39ce592437b5b5b1724466
Will Garcia <garciaw>
2009-09-15 14:55:59 +0000

    Added some command to help developers deploy with and without database install and upgrades.
    SVN-Revision: 363

--- software/build/build.cmd
+++ software/build/build.cmd
@@ -10,12 +10,13 @@ if "%1" == "" (
     echo.
     echo   clean        -- Remove classes directory for clean build
     echo   all          -- Normal build of application
-    echo   install      -- Build and deploy application
-    echo   upgrade      -- Build and upgrade application
     echo   install      -- Builds, installs JBoss and database locally
     echo   upgradewdb   -- Builds, upgrades JBoss and database locally
-    echo   dev          -- Builds, upgrades JBoss and database on DEV
-    echo   qa           -- Builds, upgrades JBoss and database on QA
+    echo   upgrade      -- Build and upgrade application
+    echo   devwdb       -- Builds, upgrades JBoss and database on DEV
+    echo   dev          -- Builds, upgrades JBoss on DEV
+    echo   qawdb        -- Builds, upgrades JBoss and database on QA
+    echo   qa           -- Builds, upgrades JBoss on QA
     echo   deploy       -- Redeploy application
     goto DONE
 )
@@ -27,32 +28,36 @@ if "%1" == "all" (
     ant build:all
     goto DONE
 )
+if "%1" == "install" (
+    ant deploy:local:install
+    goto DONE
+)
 if "%1" == "upgrade" (
     ant deploy:local:upgrade
-    goto DONE
-)
-if "%1" == "installwdb" (
-    ant -Dupgrade.target=upgrade-with-dbinstall -Denable.install.debug=true deploy:local:upgrade
-    goto DONE
-)
-if "%1" == "upgradewdb" (
-    ant -Dupgrade.target=upgrade-ncm:with-dbinstall -Denable.install.debug=true deploy:local:upgrade
-    goto DONE
-)
-if "%1" == "dev" (
-    ant -Dproperties.file=C:\SVN-Projects\ncireportwriter-properties\properties\dev-upgrade.properties -Dupgrade.target=upgrade-ncm:with-dbinstall deploy:remote:upgrade
-    goto DONE
-)
-if "%1" == "qa" (
-    ant -Dproperties.file=C:\SVN-Projects\ncireportwriter-properties\properties\qa-upgrade.properties -Dupgrade.target=upgrade-ncm:with-dbinstall deploy:remote:upgrade
     goto DONE
 )
 if "%1" == "deploy" (
     ant deploy:hot
     goto DONE
 )
-if "%1" == "install" (
-    ant deploy:local:install
+if "%1" == "upgradewdb" (
+    ant -Dupgrade.target=upgrade-ncm:with-dbinstall -Denable.install.debug=true deploy:local:upgrade
+    goto DONE
+)
+if "%1" == "devwdb" (
+    ant -Dproperties.file=C:\SVN-Projects\ncireportwriter-properties\properties\dev-upgrade.properties -Dupgrade.target=upgrade-ncm:with-dbinstall deploy:remote:upgrade
+    goto DONE
+)
+if "%1" == "dev" (
+    ant -Dproperties.file=C:\SVN-Projects\ncireportwriter-properties\properties\dev-upgrade.properties deploy:remote:upgrade
+    goto DONE
+)
+if "%1" == "qawdb" (
+    ant -Dproperties.file=C:\SVN-Projects\ncireportwriter-properties\properties\qa-upgrade.properties -Dupgrade.target=upgrade-ncm:with-dbinstall deploy:remote:upgrade
+    goto DONE
+)
+if "%1" == "qa" (
+    ant -Dproperties.file=C:\SVN-Projects\ncireportwriter-properties\properties\qa-upgrade.properties deploy:remote:upgrade
     goto DONE
 )
 if "%1" == "clean" (
