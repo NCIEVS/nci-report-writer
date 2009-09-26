b744391c73eb20ddbe0354e5e355b94e697f075c
David Yee <yeed>
2009-09-26 13:06:04 +0000

    GF23129: Reformatted FileServlet.java.
    SVN-Revision: 430

--- software/ncireportwriter/src/java/gov/nih/nci/evs/reportwriter/servlet/FileServlet.java
+++ software/ncireportwriter/src/java/gov/nih/nci/evs/reportwriter/servlet/FileServlet.java
@@ -61,243 +61,250 @@ public final class FileServlet extends HttpServlet {
 
     protected final Logger logger = Logger.getLogger(FileServlet.class);
 
-    //private SearchSessionBean searchService = null;
-	/**
-   * Validates the Init and Context parameters, configures authentication URL
-   *
-   * @throws ServletException if the init parameters are invalid or any
-   * other problems occur during initialisation
-   */
-  public void init() throws ServletException {
+    // private SearchSessionBean searchService = null;
+    /**
+     * Validates the Init and Context parameters, configures authentication URL
+     * 
+     * @throws ServletException if the init parameters are invalid or any other
+     *         problems occur during initialisation
+     */
+    public void init() throws ServletException {
 
-  }
+    }
 
-  /**
-	 * Route the user to the execute method
-   *
-   * @param request The HTTP request we are processing
-   * @param response The HTTP response we are creating
-   *
-   * @exception IOException if an input/output error occurs
-   * @exception ServletException if a servlet exception occurs
-   */
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-      execute(request, response);
-  }
+    /**
+     * Route the user to the execute method
+     * 
+     * @param request The HTTP request we are processing
+     * @param response The HTTP response we are creating
+     * 
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet exception occurs
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        execute(request, response);
+    }
 
-  /**
-   * Route the user to the execute method
-   *
-   * @param request The HTTP request we are processing
-   * @param response The HTTP response we are creating
-   *
-   * @exception IOException if an input/output error occurs
-   * @exception ServletException if a servlet exception occurs
-   */
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-      execute(request, response);
-  }
+    /**
+     * Route the user to the execute method
+     * 
+     * @param request The HTTP request we are processing
+     * @param response The HTTP response we are creating
+     * 
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet exception occurs
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        execute(request, response);
+    }
 
-  /**
-   * Process the specified HTTP request, and create the corresponding HTTP
-   * response (or forward to another web component that will create it).
-   *
-   * @param request The HTTP request we are processing
-   * @param response The HTTP response we are creating
-   *
-   * @exception IOException if an input/output error occurs
-   * @exception ServletException if a servlet exception occurs
-   */
+    /**
+     * Process the specified HTTP request, and create the corresponding HTTP
+     * response (or forward to another web component that will create it).
+     * 
+     * @param request The HTTP request we are processing
+     * @param response The HTTP response we are creating
+     * 
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet exception occurs
+     */
 
-  public void sendErrorResponse(HttpServletRequest request, HttpServletResponse response, String message) throws IOException, ServletException {
-      HttpSession session = request.getSession(false);
-      if (session != null) {
-          session.setAttribute("message", message);
+    public void sendErrorResponse(HttpServletRequest request,
+            HttpServletResponse response, String message) throws IOException,
+            ServletException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.setAttribute("message", message);
 
-		  String nextJSP = "/pages/message.jsf";
-		  RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
-		  dispatcher.forward(request, response);
-      }
-  }
+            String nextJSP = "/pages/message.jsf";
+            RequestDispatcher dispatcher =
+                getServletContext().getRequestDispatcher(nextJSP);
+            dispatcher.forward(request, response);
+        }
+    }
 
+    // String description = "Text (tab delimited)";
+    // description = "Microsoft Office Excel";
 
-//	String description = "Text (tab delimited)";
-//	description = "Microsoft Office Excel";
-
-  public void sendResponse(HttpServletResponse response, StandardReport standardReport) throws IOException, ServletException {
-		// processing download
+    public void sendResponse(HttpServletResponse response,
+            StandardReport standardReport) throws IOException, ServletException {
+        // processing download
 
         String fullPathName = standardReport.getPathName();
         ReportFormat rf = standardReport.getFormat();
         String format = rf.getDescription();
-		response.setContentType("text/html");
-		//String line_br = "\n";
+        response.setContentType("text/html");
+        // String line_br = "\n";
 
-		String line_br = "\r\n";
+        String line_br = "\r\n";
 
         response.setHeader("Cache-Control", "no-cache");
-        
-		if (format.indexOf("Excel") != -1) {
-			String contentType = "application/vnd.ms-excel";
-			response.setContentType(contentType);
-			line_br = "";
-		}
 
+        if (format.indexOf("Excel") != -1) {
+            String contentType = "application/vnd.ms-excel";
+            response.setContentType(contentType);
+            line_br = "";
+        }
 
-		File file = new File(fullPathName);
-		String filename = file.getName();
-		// HKR, GForge# 19467 - wrong placement of '\' in header.
-		response.addHeader("Content-Disposition", "attachment;filename=\"" + filename + "\"");
-		
-		System.out.println("Full path name =" + fullPathName);
-		System.out.println("File name =" + filename);
-				
-		if (format.indexOf("Excel") == -1) {
-			try {
-				FileInputStream fis = null;
-				BufferedInputStream bis = null;
-				DataInputStream dis = null;
-				try {
-				   fis = new FileInputStream(file);
-				   bis = new BufferedInputStream(fis);
-				   dis = new DataInputStream(bis);
-				   String s = "";
-				   while (dis.available() != 0) {
-					   String line = dis.readLine();
-					   s = s + line + line_br;
-				   }
-				   PrintWriter out = response.getWriter();
-				   out.write(s);
-				   out.flush();
-				   out.close();
-				} catch (Exception e) {
+        File file = new File(fullPathName);
+        String filename = file.getName();
+        // HKR, GForge# 19467 - wrong placement of '\' in header.
+        response.addHeader("Content-Disposition", "attachment;filename=\""
+                + filename + "\"");
 
-				}
+        System.out.println("Full path name =" + fullPathName);
+        System.out.println("File name =" + filename);
 
-			} catch (Exception e) {
-			}
-		}
-		else if (format.indexOf("Excel") != -1) {
-			try
-			{
-				InputStream myxls = new FileInputStream(fullPathName);
-				HSSFWorkbook wb = new HSSFWorkbook(myxls);
-				
-				try {
-					wb.write(response.getOutputStream());
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				return;
-			} catch (Exception e) {
-				//LogUtils.log(logger, Level.ERROR, e);
-			}
-		}
-  }
+        if (format.indexOf("Excel") == -1) {
+            try {
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                DataInputStream dis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    dis = new DataInputStream(bis);
+                    String s = "";
+                    while (dis.available() != 0) {
+                        String line = dis.readLine();
+                        s = s + line + line_br;
+                    }
+                    PrintWriter out = response.getWriter();
+                    out.write(s);
+                    out.flush();
+                    out.close();
+                } catch (Exception e) {
 
+                }
 
-  public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+            } catch (Exception e) {
+            }
+        } else if (format.indexOf("Excel") != -1) {
+            try {
+                InputStream myxls = new FileInputStream(fullPathName);
+                HSSFWorkbook wb = new HSSFWorkbook(myxls);
 
-//<td class="dataCellText"><a href="<%=request.getContextPath() %>/fileServlet?template=<%=templateId%>&format=<%=formatId%>" ><%=template%> (<%=formatDescription%>)</a></td>
-		// Get ReportFormat from database
-		String formatId = request.getParameter("format");
-		String templateId = request.getParameter("template");
-		ReportFormat reportFormat = null;
-		StandardReportTemplate standardReportTemplate = null;
-		String message = "Format ID " + formatId + " not found.";
-		try {
-			reportFormat = null;
-			String FQName = "gov.nih.nci.evs.reportwriter.bean.ReportFormat";
-			String methodName = "setId";
-			int format_id = Integer.parseInt(formatId);
-			SDKClientUtil sdkclientutil = new SDKClientUtil();
-			Object reportFormat_obj = sdkclientutil.search(FQName, methodName, format_id);
-			if (reportFormat_obj == null) {
-				sendErrorResponse(request, response, message);
-				return;
-			}
-			else
-			{
-				reportFormat = (ReportFormat) reportFormat_obj;
-			}
-			// Get StandardReportTemplate from database
-			message = "Template ID " + templateId + " not found.";
-			try {
-				// Get all gov.nih.nci.evs.reportwriter.bean.StandardReportTemplate
-				// Find ov.nih.nci.evs.reportwriter.bean.StandardReport with matched template and format
-				// Find the fullpath of the StandardReport
-				// Check ReportStatus (send not available or pending approval message as needed)
+                try {
+                    wb.write(response.getOutputStream());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return;
+            } catch (Exception e) {
+                // LogUtils.log(logger, Level.ERROR, e);
+            }
+        }
+    }
 
-				FQName = "gov.nih.nci.evs.reportwriter.bean.StandardReportTemplate";
-				methodName = "setId";
-				int template_id = Integer.parseInt(templateId);
-				Object standardReportTemplate_obj = sdkclientutil.search(FQName, methodName, template_id);
-				if (standardReportTemplate_obj == null) {
-					sendErrorResponse(request, response, message);
-				}
+    public void execute(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
 
-				standardReportTemplate = (StandardReportTemplate) standardReportTemplate_obj;
+        // <td class="dataCellText"><a
+        // href="<%=request.getContextPath() %>/fileServlet?template=<%=templateId%>&format=<%=formatId%>"
+        // ><%=template%> (<%=formatDescription%>)</a></td>
+        // Get ReportFormat from database
+        String formatId = request.getParameter("format");
+        String templateId = request.getParameter("template");
+        ReportFormat reportFormat = null;
+        StandardReportTemplate standardReportTemplate = null;
+        String message = "Format ID " + formatId + " not found.";
+        try {
+            reportFormat = null;
+            String FQName = "gov.nih.nci.evs.reportwriter.bean.ReportFormat";
+            String methodName = "setId";
+            int format_id = Integer.parseInt(formatId);
+            SDKClientUtil sdkclientutil = new SDKClientUtil();
+            Object reportFormat_obj =
+                sdkclientutil.search(FQName, methodName, format_id);
+            if (reportFormat_obj == null) {
+                sendErrorResponse(request, response, message);
+                return;
+            } else {
+                reportFormat = (ReportFormat) reportFormat_obj;
+            }
+            // Get StandardReportTemplate from database
+            message = "Template ID " + templateId + " not found.";
+            try {
+                // Get all
+                // gov.nih.nci.evs.reportwriter.bean.StandardReportTemplate
+                // Find ov.nih.nci.evs.reportwriter.bean.StandardReport with
+                // matched template and format
+                // Find the fullpath of the StandardReport
+                // Check ReportStatus (send not available or pending approval
+                // message as needed)
 
-				// Search for the matched StandardReport in the database (by template and format)
-				FQName = "gov.nih.nci.evs.reportwriter.bean.StandardReport";
-				message = "The selected report is not available for download.";
-				Object[] objs = sdkclientutil.search(FQName);
+                FQName =
+                    "gov.nih.nci.evs.reportwriter.bean.StandardReportTemplate";
+                methodName = "setId";
+                int template_id = Integer.parseInt(templateId);
+                Object standardReportTemplate_obj =
+                    sdkclientutil.search(FQName, methodName, template_id);
+                if (standardReportTemplate_obj == null) {
+                    sendErrorResponse(request, response, message);
+                }
 
-				if (objs == null || objs.length == 0)
-				{
-					sendErrorResponse(request, response, message);
-				}
-				else
-				{
-					for (int i=0; i<objs.length; i++)
-					{
-						StandardReport standardReport = (StandardReport) objs[i];
-						StandardReportTemplate srt = standardReport.getTemplate();
+                standardReportTemplate =
+                    (StandardReportTemplate) standardReportTemplate_obj;
+
+                // Search for the matched StandardReport in the database (by
+                // template and format)
+                FQName = "gov.nih.nci.evs.reportwriter.bean.StandardReport";
+                message = "The selected report is not available for download.";
+                Object[] objs = sdkclientutil.search(FQName);
+
+                if (objs == null || objs.length == 0) {
+                    sendErrorResponse(request, response, message);
+                } else {
+                    for (int i = 0; i < objs.length; i++) {
+                        StandardReport standardReport =
+                            (StandardReport) objs[i];
+                        StandardReportTemplate srt =
+                            standardReport.getTemplate();
                         int i1 = srt.getId().intValue();
                         int i2 = standardReportTemplate.getId().intValue();
 
-						if (i1 == i2)
-						{
-							ReportFormat rf = standardReport.getFormat();
-							if (rf != null && rf.getId() == format_id)
-							{
-								HttpSession session = request.getSession(false);
-								Boolean isAdmin = (Boolean) session.getAttribute("isAdmin");
-								if (isAdmin != null && isAdmin.equals(Boolean.TRUE))
-								{
-									sendResponse(response, standardReport);
-								}
-								else
-								{
-									//Check if the report has been approved:
-									ReportStatus rs = standardReport.getStatus();
-									if (rs != null) {
-										String approved = "APPROVED";
-										if (approved.equals(rs.getLabel()))
-										{
-											sendResponse(response, standardReport);
-										}
-										else
-										{
-											sendErrorResponse(request, response, "The selected report has not yet been approved.");
-										}
-									} else {
-										sendErrorResponse(request, response, "The selected report has not yet been approved.");
-									}
-								}
-							}
-						}
-					}
-			    }
-			} catch (Exception ex) {
-				sendErrorResponse(request, response, message);
-				ex.printStackTrace();
-			}
+                        if (i1 == i2) {
+                            ReportFormat rf = standardReport.getFormat();
+                            if (rf != null && rf.getId() == format_id) {
+                                HttpSession session = request.getSession(false);
+                                Boolean isAdmin =
+                                    (Boolean) session.getAttribute("isAdmin");
+                                if (isAdmin != null
+                                        && isAdmin.equals(Boolean.TRUE)) {
+                                    sendResponse(response, standardReport);
+                                } else {
+                                    // Check if the report has been approved:
+                                    ReportStatus rs =
+                                        standardReport.getStatus();
+                                    if (rs != null) {
+                                        String approved = "APPROVED";
+                                        if (approved.equals(rs.getLabel())) {
+                                            sendResponse(response,
+                                                    standardReport);
+                                        } else {
+                                            sendErrorResponse(request,
+                                                    response,
+                                                    "The selected report has not yet been approved.");
+                                        }
+                                    } else {
+                                        sendErrorResponse(request, response,
+                                                "The selected report has not yet been approved.");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (Exception ex) {
+                sendErrorResponse(request, response, message);
+                ex.printStackTrace();
+            }
 
-		} catch (Exception ex) {
-			sendErrorResponse(request, response, message);
-			ex.printStackTrace();
-		}
+        } catch (Exception ex) {
+            sendErrorResponse(request, response, message);
+            ex.printStackTrace();
+        }
     }
 }
-
