eb74e865a35e62573ea9287d93ac981b36ab9a11
David Yee <yeed>
2009-11-03 16:12:21 +0000

    GF23067: Cleaned up test programs.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncirw/trunk@605 e1e6a9b6-88fc-49c9-b504-51d68f5d0411

--- software/ncireportwriter/test/bin/run.bat
+++ software/ncireportwriter/test/bin/run.bat
@@ -15,7 +15,7 @@ set CLASSPATH=%cp%
 
 set java=%JAVA_HOME%\bin\java
 set class=gov.nih.nci.evs.reportwriter.test.lexevs.TestRemoteServerUtil
-set args
+set args=-propertyFile C:/apps/evs/ncireportwriter-webapp/conf/ncireportwriter.properties
 
 rem -------------------------------------------------------------------------------------
 @echo on
--- software/ncireportwriter/test/src/java/gov/nih/nci/evs/reportwriter/test/lexevs/TestEnv.java
+++ software/ncireportwriter/test/src/java/gov/nih/nci/evs/reportwriter/test/lexevs/TestEnv.java
@@ -1,5 +1,7 @@
 package gov.nih.nci.evs.reportwriter.test.lexevs;
 
+import java.util.*;
+
 public class TestEnv {
     public static final String RW_PROPERTY_FILE = 
         "gov.nih.nci.cacore.ncireportwriterProperties";
@@ -10,7 +12,20 @@ public class TestEnv {
         System.setProperty(RW_PROPERTY_FILE, propertyFile);
     }
 
-    public static void setup() {
-        setup(_propertyFile);
+    public static String[] parse(String[] args) {
+        String prevArg = "";
+        ArrayList<String> newArgs = new ArrayList<String>();
+        for (int i = 0; i < args.length; ++i) {
+            String arg = args[i];
+            if (arg.equals("-propertyFile")) {
+                prevArg = arg;
+            } else if (prevArg.equals("-propertyFile")) {
+                setup(arg);
+                prevArg = "";
+            } else {
+                newArgs.add(arg);
+            }
+        }
+        return newArgs.toArray(new String[newArgs.size()]);
     }
 }
