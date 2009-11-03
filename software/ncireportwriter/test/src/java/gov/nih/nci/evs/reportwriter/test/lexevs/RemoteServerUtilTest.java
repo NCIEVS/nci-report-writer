e2d50499aac16e5b80392c7a14e247fdf074ce07
David Yee <yeed>
2009-11-03 19:21:38 +0000

    GF23067: Renamed TestRemoteServerUtil to RemoteServerUtilTest.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncirw/trunk@614 e1e6a9b6-88fc-49c9-b504-51d68f5d0411

--- software/ncireportwriter/test/src/java/gov/nih/nci/evs/reportwriter/test/lexevs/RemoteServerUtilTest.java
+++ software/ncireportwriter/test/src/java/gov/nih/nci/evs/reportwriter/test/lexevs/RemoteServerUtilTest.java
@@ -5,12 +5,12 @@ import org.apache.log4j.*;
 import gov.nih.nci.evs.reportwriter.utils.*;
 import junit.framework.*;
 
-public class TestRemoteServerUtil extends TestCase {
-    private static Logger _logger = Logger.getLogger(TestRemoteServerUtil.class);
+public class RemoteServerUtilTest extends TestCase {
+    private static Logger _logger = Logger.getLogger(RemoteServerUtilTest.class);
     
     public static void main(String[] args) {
         args = TestEnv.parse(args);
-        TestRemoteServerUtil test = new TestRemoteServerUtil();
+        RemoteServerUtilTest test = new RemoteServerUtilTest();
         test.test_createLexBIGService();
     }
     
