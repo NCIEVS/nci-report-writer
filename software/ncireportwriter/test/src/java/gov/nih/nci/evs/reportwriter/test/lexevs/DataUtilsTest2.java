2844033191f2eae653f98182d4f314622cccc941
David Yee <yeed>
2010-02-09 12:48:17 +0000

    GF23067: Created DataUtils2 junit that retrieves supported associations.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncirw/trunk@693 e1e6a9b6-88fc-49c9-b504-51d68f5d0411

---
+++ software/ncireportwriter/test/src/java/gov/nih/nci/evs/reportwriter/test/lexevs/DataUtilsTest2.java
@@ -0,0 +1,39 @@
+package gov.nih.nci.evs.reportwriter.test.lexevs;
+
+import java.util.*;
+
+import gov.nih.nci.evs.reportwriter.test.utils.*;
+import gov.nih.nci.evs.reportwriter.utils.*;
+
+import org.apache.log4j.*;
+
+public class DataUtilsTest2 {
+    private static Logger _logger = Logger.getLogger(DataUtilsTest2.class);
+
+    public static void main(String[] args) {
+        args = SetupEnv.getInstance().parse(args);
+        DataUtilsTest2 test = new DataUtilsTest2();
+        test.getSupportedAssociationNames();
+    }
+    
+    public void getSupportedAssociationNames()
+    {
+        String scheme = "NCI Thesaurus";
+        String version = "09.12d";
+
+        Vector<String> values;
+        try {
+            values = DataUtils.getSupportedAssociations(
+                DataUtils.AssociationType.Codes, scheme, version);
+            Iterator<String> iterator = values.iterator();
+            int i=0;
+            while (iterator.hasNext()) {
+                String value = iterator.next();
+                _logger.info(i + ") " + value);
+                ++i;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
