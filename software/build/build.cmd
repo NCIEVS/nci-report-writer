bfe1c292a0155110eae199e9a1a3453f5e6f22d8
Will Garcia <garciaw>
2010-02-22 18:39:09 +0000

    Fixed type-o with upgradewdb and rename targets for readability.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncirw/trunk@780 e1e6a9b6-88fc-49c9-b504-51d68f5d0411

--- software/build/build.cmd
+++ software/build/build.cmd
@@ -8,20 +8,20 @@ echo.
 setlocal
 set DEVPROPFILE=C:\SVN-Projects\ncireportwriter-properties\properties\dev-upgrade.properties
 set QAPROPFILE=C:\SVN-Projects\ncireportwriter-properties\properties\qa-upgrade.properties
-if "%1" == "" (
+if "%1" == ""  (
     echo.
     echo Available targets are:
     echo.
-    echo   clean        -- Remove classes directory for clean build
-    echo   all          -- Normal build of application
-    echo   install      -- Builds, installs JBoss and database locally
-    echo   upgradewdb   -- Builds, upgrades JBoss and database locally
-    echo   upgrade      -- Build and upgrade application
-    echo   devwdb       -- Builds, upgrades JBoss and database on DEV
-    echo   dev          -- Builds, upgrades JBoss on DEV
-    echo   qawdb        -- Builds, upgrades JBoss and database on QA
-    echo   qa           -- Builds, upgrades JBoss on QA
-    echo   deploy       -- Redeploy application
+    echo   1. clean                -- Remove classes directory for clean build
+    echo   2. all                  -- Normal build of application
+    echo   3. install              -- Builds, installs JBoss and database locally
+    echo   4. upgrade:wdbinstall   -- Builds, upgrades JBoss and installs database locally
+    echo   5. upgrade              -- Build and upgrade application
+    echo   6. dev:wdbinstall       -- Builds, upgrades JBoss and installs database on DEV
+    echo   7. dev                  -- Builds, upgrades JBoss on DEV
+    echo   8. qa:wdbinstall        -- Builds, upgrades JBoss and install database on QA
+    echo   9. qa                   -- Builds, upgrades JBoss on QA
+    echo  10. deploy               -- Redeploy application locally
     goto DONE
 )
 if "%1" == "usage" (
@@ -44,15 +44,15 @@ if "%1" == "deploy" (
     ant deploy:hot
     goto DONE
 )
-if "%1" == "upgradewdb" (
-    ant -Dupgrade.target=upgrade-ncm:with-dbinstall -Denable.install.debug=true -Ddatabase.re-create deploy:local:upgrade
+if "%1" == "upgrade:wdbinstall" (
+    ant -Dupgrade.target=upgrade-ncm:with-dbinstall -Denable.install.debug=true -Ddatabase.re-create=true deploy:local:upgrade
     goto DONE
 )
 if "%1" == "dev" (
     ant -Dproperties.file=%DEVPROPFILE% deploy:remote:upgrade
     goto DONE
 )
-if not "%1" == "devwdb" goto d1
+if not "%1" == "dev:wdbinstall" goto d1
     @rem *** Remember to set database.drop-schema=true in dev-upgrade.properties file ***
     type %DEVPROPFILE% | findstr "database.drop-schema=true" >nul
     if "%errorlevel%" == "0" goto d2
@@ -69,7 +69,7 @@ if "%1" == "qa" (
     ant -Dproperties.file=%QAPROPFILE% deploy:remote:upgrade
     goto DONE
 )
-if not "%1" == "qawdb" goto q1
+if not "%1" == "qa:wdbinstall" goto q1
     @rem *** Remember to set database.drop-schema=true in qa-upgrade.properties file ***
     type %QAPROPFILE% | findstr "database.drop-schema=true" >nul
     if "%errorlevel%" == "0" goto q2
