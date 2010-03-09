74c09be274d88b604a5e04e072c1d90ed2312f78
David Yee <yeed>
2010-03-09 16:04:26 +0000

    GF26858: Modification to suppress rows within codelist extensible value.   * Added subrow header or common codelist code values.
    SVN-Revision: 823

--- software/ncireportwriter/test/src/java/gov/nih/nci/evs/reportwriter/test/utils/FileUtilTest.java
+++ software/ncireportwriter/test/src/java/gov/nih/nci/evs/reportwriter/test/utils/FileUtilTest.java
@@ -1,15 +1,18 @@
 package gov.nih.nci.evs.reportwriter.test.utils;
 
 import gov.nih.nci.evs.reportwriter.utils.*;
+import org.apache.log4j.*;
 
 public class FileUtilTest {
+    private static Logger _logger = Logger.getLogger(FileUtilTest.class);
+    
     public static void main(String[] args) {
         try {
             String dir = "C:/apps/evs/ncireportwriter-webapp/downloads";
-            String txtFile = dir + "/CDRH_Subset_REPORT__09.12d.txt";
-            String xlsFile = dir + "/CDRH_Subset_REPORT__09.12d_test.xls";
+            String txtFile = dir + "/CDISC_SDTM_Terminology__09.12d.txt";
             String delimiter = "\t";
-            FileUtil.convertToExcel(txtFile, delimiter,xlsFile);
+            FileUtil.convertToExcel(txtFile, delimiter);
+            _logger.debug("Done.");
         } catch (Exception e) {
             e.printStackTrace();
         }
