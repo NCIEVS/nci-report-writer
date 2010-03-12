458c8fb45e0c70ae84a6ab02ec5a8e4d03a8862f
David Yee <yeed>
2010-03-12 14:54:23 +0000

    GF23129: Moved all report content action methods from UserSessionBean   to newly created ReportContentRequest class.
    SVN-Revision: 842

--- software/ncireportwriter/src/java/gov/nih/nci/evs/reportwriter/bean/UserSessionBean.java
+++ software/ncireportwriter/src/java/gov/nih/nci/evs/reportwriter/bean/UserSessionBean.java
@@ -4,7 +4,6 @@ import java.io.*;
 
 import gov.nih.nci.evs.reportwriter.utils.*;
 import gov.nih.nci.evs.reportwriter.webapp.*;
-import gov.nih.nci.evs.reportwriter.service.*;
 import gov.nih.nci.evs.reportwriter.properties.*;
 
 import java.util.*;
@@ -14,9 +13,6 @@ import javax.faces.model.*;
 import javax.servlet.http.*;
 
 import org.apache.log4j.*;
-
-import org.LexGrid.codingSchemes.*;
-import org.LexGrid.concepts.*;
 
 /**
  * <!-- LICENSE_TEXT_START -->
@@ -508,193 +504,6 @@ public class UserSessionBean extends Object {
         return "assign_report_status";
     }
 
-    public String editReportContentAction() {
-        HttpServletRequest request = SessionUtil.getRequest();
-        request.getSession().setAttribute("selectedStandardReportTemplate",
-            _selectedStandardReportTemplate);
-
-        StandardReportTemplate standardReportTemplate =
-            getStandardReportTemplate(_selectedStandardReportTemplate);
-        String csn = standardReportTemplate.getCodingSchemeName();
-        String version = standardReportTemplate.getCodingSchemeVersion();
-        String csnv = DataUtils.getCSNVKey(csn, version);
-        request.getSession().setAttribute("selectedOntology", csnv);
-
-        String warningMsg = displayCodingSchemeWarning(request);
-        return warningMsg == null ? "standard_report_column" : warningMsg;
-    }
-
-    public String displayCodingSchemeWarning(HttpServletRequest request) {
-        StandardReportTemplate standardReportTemplate =
-            getStandardReportTemplate(_selectedStandardReportTemplate);
-        String csn = standardReportTemplate.getCodingSchemeName();
-        String version = standardReportTemplate.getCodingSchemeVersion();
-        String csnv = DataUtils.getCSNVKey(csn, version);
-
-        String versionTmp = DataUtils.getCodingSchemeVersion(csnv);
-        if (versionTmp != null)
-            return null;
-
-        CodingScheme cs = DataUtils.getCodingScheme(csn);
-        if (cs == null)
-            return HTTPUtils.warningMsg(request,
-                "The following vocabulary is not loaded:\n" + "    * " + csnv);
-
-        versionTmp = cs.getRepresentsVersion();
-        String csnvLatest = DataUtils.getCSNVKey(csn, versionTmp);
-        String msg = "";
-        msg +=
-            "The selected report template is referencing an older or invalid version of the coding scheme:\n";
-        msg += "    * Current version: " + csnv + "\n";
-        msg += "    * Latest version: " + csnvLatest + "\n";
-        msg += "\n";
-        msg +=
-            "Please update the version number of the coding scheme by selecting the Modify button.";
-        return HTTPUtils.warningMsg(request, msg);
-    }
-
-    public String generateStandardReportAction() {
-        HttpServletRequest request = SessionUtil.getRequest();
-
-        String warningMsg = displayCodingSchemeWarning(request);
-        if (warningMsg != null)
-            return warningMsg;
-
-        String templateId =
-            (String) request.getSession().getAttribute(
-                "selectedStandardReportTemplate");
-
-        _logger.debug("generateStandardReportAction: " + templateId);
-
-        // boolean set_defined_by_code = true;
-        String defining_set_desc = null;
-        try {
-            SDKClientUtil sdkclientutil = new SDKClientUtil();
-            StandardReportTemplate standardReportTemplate = null;
-            String FQName =
-                "gov.nih.nci.evs.reportwriter.bean.StandardReportTemplate";
-            String methodName = "setLabel";
-            String key = templateId;
-            Object standardReportTemplate_obj =
-                sdkclientutil.search(FQName, methodName, key);
-            if (standardReportTemplate_obj != null) {
-                standardReportTemplate =
-                    (StandardReportTemplate) standardReportTemplate_obj;
-
-                String codingscheme =
-                    standardReportTemplate.getCodingSchemeName();
-                String version =
-                    standardReportTemplate.getCodingSchemeVersion();
-
-                _logger.debug("generateStandardReportAction: codingscheme "
-                    + codingscheme);
-                _logger.debug("generateStandardReportAction: version "
-                    + version);
-
-                if (!DataUtils.isValidCodingScheme(codingscheme, version)) {
-                    String message =
-                        "Invalid coding scheme name "
-                            + codingscheme
-                            + " or version "
-                            + version
-                            + " -- The report template may be out of date. Please modify it and resubmit.";
-                    request.getSession().setAttribute("message", message);
-                    return "message";
-                }
-
-                defining_set_desc = standardReportTemplate.getRootConceptCode();
-                String rootConceptCode = null;
-                if (defining_set_desc.indexOf("|") == -1) {
-                    rootConceptCode =
-                        standardReportTemplate.getRootConceptCode();
-                    String ltag = null;
-                    Concept rootConcept =
-                        DataUtils.getConceptByCode(codingscheme, version, ltag,
-                            rootConceptCode);
-                    if (rootConcept == null) {
-                        String message =
-                            "Invalid root concept code "
-                                + rootConceptCode
-                                + " -- Please modify the report template and resubmit.";
-                        request.getSession().setAttribute("message", message);
-                        return "message";
-                    }
-                    String associationName =
-                        standardReportTemplate.getAssociationName();
-                    key = codingscheme + " (version: " + version + ")";
-                    Vector<String> associationname_vec =
-                        DataUtils.getSupportedAssociations(
-                            DataUtils.AssociationType.Names, key);
-                    if (!associationname_vec.contains(associationName)) {
-                        String message =
-                            "Invalid association name "
-                                + associationName
-                                + " -- Please modify the report template and resubmit.";
-                        request.getSession().setAttribute("message", message);
-                        return "message";
-                    }
-                } else {
-                    // set_defined_by_code = false;
-                }
-            }
-        } catch (Exception ex) {
-            String message =
-                "Exception encountered when generating " + templateId + ".";
-            request.getSession().setAttribute("message", message);
-            return "message";
-        }
-
-        String uid = (String) request.getSession().getAttribute("uid");
-        if (uid == null) {
-            String message = "You must first login to perform this function.";
-            request.getSession().setAttribute("message", message);
-            return "message";
-        }
-
-        String reportFormat_value = "Text (tab delimited)";
-        String reportStatus_value = "DRAFT";
-
-        String message =
-            StandardReportService.validReport(_selectedStandardReportTemplate,
-                reportFormat_value, reportStatus_value, uid);
-
-        if (message.compareTo("success") != 0) {
-            request.getSession().setAttribute("message", message);
-            return "message";
-        }
-
-        String download_dir =
-            AppProperties.getInstance().getProperty(
-                AppProperties.REPORT_DOWNLOAD_DIRECTORY);
-
-        _logger.debug("download_dir " + download_dir);
-        if (download_dir == null) {
-            message =
-                "The download directory has not been set up properly -- ask your administrator to check JBoss setting in properties-service.xml.";
-            request.getSession().setAttribute("message", message);
-            return "message";
-        }
-
-        String emailAddress =
-            (String) request.getSession().getAttribute("email");
-        _logger.debug("emailAddress: " + emailAddress);
-        StandardReportService.generateStandardReport(download_dir,
-            _selectedStandardReportTemplate, uid, emailAddress);
-
-        message =
-            "You request has been received. The report, "
-                + templateId
-                + ", in tab-delimited and Microsft Excel formats will be generated and placed in the designated output directory."
-                + " Please review and assign an APPROVED status before making it available to the users.";
-        if (emailAddress != null && emailAddress.length() > 0) {
-            message +=
-                "\n\nOnce the report is generated, an email notification will be sent to "
-                    + emailAddress + ".";
-        }
-        request.getSession().setAttribute("message", message);
-        return "message"; // replaced by a messsage page (back button)
-    }
-
     public String downloadReportAction() {
         HttpServletRequest request = SessionUtil.getRequest();
         request.getSession().setAttribute("selectedStandardReportTemplate",
@@ -871,6 +680,17 @@ public class UserSessionBean extends Object {
     public String saveModifiedReportColumnAction() {
         return new ReportColumnRequest(_selectedStandardReportTemplate)
             .saveModifiedAction();
+    }
+    
+    // -------------------------------------------------------------------------
+    public String editReportContentAction() {
+        return new ReportContentRequest(_selectedStandardReportTemplate)
+            .editAction();
+    }
+
+    public String generateStandardReportAction() {
+        return new ReportContentRequest(_selectedStandardReportTemplate)
+            .generateAction();
     }
 
     // -------------------------------------------------------------------------
