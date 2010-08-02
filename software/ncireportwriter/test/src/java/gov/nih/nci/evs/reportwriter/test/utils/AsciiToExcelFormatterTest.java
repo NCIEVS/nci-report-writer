842ca9606fc26ac635c28ba548365882088104f7
David Yee <yeed>
2010-08-02 19:15:21 +0000

    GF29243: Renamed FileUtil class to AsciiToExcelFormatter.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncirw/branches/2010-07-29.migration@909 e1e6a9b6-88fc-49c9-b504-51d68f5d0411

--- software/ncireportwriter/test/src/java/gov/nih/nci/evs/reportwriter/test/utils/AsciiToExcelFormatterTest.java
+++ software/ncireportwriter/test/src/java/gov/nih/nci/evs/reportwriter/test/utils/AsciiToExcelFormatterTest.java
@@ -3,15 +3,15 @@ package gov.nih.nci.evs.reportwriter.test.utils;
 import gov.nih.nci.evs.reportwriter.utils.*;
 import org.apache.log4j.*;
 
-public class FileUtilTest {
-    private static Logger _logger = Logger.getLogger(FileUtilTest.class);
+public class AsciiToExcelFormatterTest {
+    private static Logger _logger = Logger.getLogger(AsciiToExcelFormatterTest.class);
     
     public static void main(String[] args) {
         try {
             String dir = "C:/apps/evs/ncireportwriter-webapp/downloads";
             String txtFile = dir + "/CDISC_SDTM_Terminology__09.12d.txt";
             String delimiter = "\t";
-            new FileUtil().convert(txtFile, delimiter);
+            new AsciiToExcelFormatter().convert(txtFile, delimiter);
             _logger.debug("Done.");
         } catch (Exception e) {
             e.printStackTrace();
