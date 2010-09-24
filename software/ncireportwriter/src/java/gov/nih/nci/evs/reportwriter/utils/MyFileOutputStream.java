4892da8c2be5053d32b9068ea9486de990c2e86f
David Yee <yeed>
2010-09-24 13:12:19 +0000

    GF29761(NCIt): First pass at modifying text formatter classes to be generic   enough to be used by Term Browser/NCIt.
    SVN-Revision: 1008

--- software/ncireportwriter/src/java/gov/nih/nci/evs/reportwriter/utils/MyFileOutputStream.java
+++ software/ncireportwriter/src/java/gov/nih/nci/evs/reportwriter/utils/MyFileOutputStream.java
@@ -2,12 +2,10 @@ package gov.nih.nci.evs.reportwriter.utils;
 
 import java.io.*;
 
-public class MyFileOutputStream {
-	private static final String TAB = "  ";
+public class MyFileOutputStream extends TabFormatterBase 
+{
 	private String _filename = null;
 	private FileOutputStream _out = null;
-	private String _tab = TAB;
-	private String _indentation = "";
 
 	public MyFileOutputStream(String filename) throws Exception {
 		_filename = filename;
@@ -24,54 +22,10 @@ public class MyFileOutputStream {
 		_out.close();
 	}
 
-	public void setTab(String tab) {
-		_tab = tab;
-	}
-
-	public void write(String text) throws Exception {
-	    if (text == null)
-	        text = "";
-	    text = _indentation + text;
-	    //text = String.format("%4d: ", _indentation.length()) + text;
+    public String write(String text) throws Exception {
+        text = super.write(text);
         _out.write(text.getBytes());
-	}
+        return text;
+    }
 
-	public void writeln(String text) throws Exception {
-	    write(text + "\n");
-	}
-	
-	public void writeln() throws Exception {
-		writeln("");
-	}
-
-	public String indent() {
-		_indentation += _tab;
-		return _indentation;
-	}
-
-	public String undent() {
-		if (_indentation.length() > 0)
-			_indentation = _indentation.substring(_tab.length());
-		return _indentation;
-	}
-
-	public void writeln_indent(String text) throws Exception {
-	    indent();
-		writeln(text);
-	}
-
-	public void writeln_undent(String text) throws Exception {
-		writeln(text);
-        undent();
-	}
-
-	public void writeln_normal(String text) throws Exception {
-		writeln(text);
-	}
-	
-	public void writeln_inden1(String text) throws Exception {
-	    indent();
-        writeln(text);
-        undent();
-	}
 }
