b88f01c02abb0b22a48b60adc4598b56abbd9319
David Yee <yeed>
2010-09-28 17:32:56 +0000

    GF29761: Created FormatterConstant interface.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncirw/trunk@1021 e1e6a9b6-88fc-49c9-b504-51d68f5d0411

--- software/ncireportwriter/src/java/gov/nih/nci/evs/reportwriter/formatter/AsciiToTextFormatter.java
+++ software/ncireportwriter/src/java/gov/nih/nci/evs/reportwriter/formatter/AsciiToTextFormatter.java
@@ -54,54 +54,65 @@ import org.apache.log4j.*;
  * @version 1.0
  */
 
-public class AsciiToTextFormatter extends FileFormatterBase {
-	private static Logger _logger = Logger
-			.getLogger(AsciiToTextFormatter.class);
+public class AsciiToTextFormatter extends FileFormatterBase implements
+        FormatterConstant {
+    private static Logger _logger = Logger
+        .getLogger(AsciiToTextFormatter.class);
 
-	public Boolean convert(String textfile, String delimiter) throws Exception {
-		return convert2(textfile, "edt", delimiter);
-	}
+    public Boolean convert(String textfile, String delimiter) throws Exception {
+        return convert2(textfile, "edt", delimiter);
+    }
 
-	public Boolean convert(String textfile, String delimiter, String outfile)
-			throws Exception {
-		BufferedReader br = getBufferReader(textfile);
-		TabFormatterFileOutputStream out = new TabFormatterFileOutputStream(outfile);
-		Vector<String> headings = getColumnHeadings(textfile, delimiter);
+    public Boolean convert(String textfile, String delimiter, String outfile)
+            throws Exception {
+        BufferedReader br = getBufferReader(textfile);
+        TabFormatterFileOutputStream out =
+            new TabFormatterFileOutputStream(outfile);
+        Vector<String> headings = getColumnHeadings(textfile, delimiter);
 
-		int row = 0;
-		while (true) {
-			String line = br.readLine();
-			if (line == null)
-				break;
-			if (line.length() <= 0)
-				continue;
+        int row = 0;
+        while (true) {
+            String line = br.readLine();
+            if (line == null)
+                break;
+            if (line.length() <= 0)
+                continue;
 
-			if (row > 0) {
-				Vector<String> v = parseData(line, delimiter);
-				out.writeln(StringUtils.SEPARATOR);
-				for (int col = 0; col < v.size(); col++) {
-					out.writeln("[" + row + "," + col + "] "
-							+ headings.get(col) + ": " + v.get(col));
-				}
-				out.writeln();
-			}
-			row++;
-		}
-		br.close();
-		out.close();
-		return Boolean.TRUE;
-	}
+            if (row > 0) {
+                Vector<String> v = parseData(line, delimiter);
+                out.writeln(StringUtils.SEPARATOR);
+                for (int col = 0; col < v.size(); col++) {
+                    out.writeln("[" + row + "," + col + "] "
+                        + headings.get(col) + ": " + v.get(col));
+                }
+                out.writeln();
+            }
+            row++;
+        }
+        br.close();
+        out.close();
+        return Boolean.TRUE;
+    }
 
-	public static void main(String[] args) {
-		try {
-			String dir = "C:/apps/evs/ncireportwriter-webapp/downloads";
-			String infile = dir + "/FDA-SPL_Country_Code_REPORT__10.06e.txt";
-			String delimiter = "\t";
+    // -- Test Program- --------------------------------------------------------
+    public static void test(String textfile, int[] ncitCodeColumns) {
+        try {
+            String delimiter = "\t";
+            AsciiToTextFormatter formatter = new AsciiToTextFormatter();
+            formatter.convert(textfile, delimiter);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-			new AsciiToTextFormatter().convert(infile, delimiter);
-			_logger.debug("Done");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        test(CDISC_SDTM_FILE, CDISC_SDTM_NCIT_COLUMNS);
+        test(CDISC_SUBSET_FILE, CDISC_SUBSET_NCIT_COLUMNS);
+        test(CDRH_SUBSET_FILE, CDRH_COLUMNS);
+        test(FDA_SPL_FILE, FDA_SPL_NCIT_COLUMNS);
+        test(FDA_UNII_FILE, FDA_UNII_NCIT_COLUMNS);
+        test(ICS_SUBSET_FILE, ICS_SUBSET_NCIT_COLUMNS);
+        test(SPL_FILE, SPL_NCIT_COLUMNS);
+        _logger.debug("Done");
+    }
 }
