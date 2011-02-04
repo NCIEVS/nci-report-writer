5fc708329c27de9c76df0f4325f061b7158c885d
Will Garcia <garciawa2>
2011-02-04 13:34:20 +0000

    Upgrading to BDA-Lite.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncirw/trunk@1081 e1e6a9b6-88fc-49c9-b504-51d68f5d0411

--- software/build/build.cmd
+++ software/build/build.cmd
@@ -1,136 +1,63 @@
 @echo off
-rem cls
-echo.
-echo ******************************************
-echo **** Command file to invoke build.xml ****
-echo ******************************************
-echo.
+@rem ******************************************
+@rem **** Command file to invoke build.xml ****
+@rem ******************************************
 setlocal
 @rem Environment settings here...
-set DEVPROPFILE=C:\NCI-Projects\ncireportwriter-properties\properties\dev-upgrade.properties
-set CIPROPFILE=C:\NCI-Projects\ncireportwriter-properties\properties\ci-upgrade.properties
-set QAPROPFILE=C:\NCI-Projects\ncireportwriter-properties\properties\qa-upgrade.properties
-set DATAQAPROPFILE=C:\NCI-Projects\ncireportwriter-properties\properties\data-qa-upgrade.properties
 set DEBUG=-Denable.install.debug=false
 set TAG=-Danthill.build.tag_built=desktop
 @rem Test is debug has been set
 if "%2" == "debug" (
-    set DEBUG=-Denable.install.debug=true -debug
+    set DEBUG=-debug
 )
-if "%1" == ""  (
+cls
+if "%1" == "" (
     echo.
     echo Available targets are:
     echo.
-    echo   clean                -- Remove classes directory for clean build
-    echo   all                  -- Normal build of application
-    echo   install              -- Builds, installs JBoss and database locally
-    echo   upgrade:wdbinstall   -- Builds, upgrades JBoss and installs database locally
-    echo   upgrade              -- Build and upgrade application
-    echo   dev:wdbinstall       -- Builds, upgrades JBoss and installs database on DEV
-    echo   dev                  -- Builds, upgrades JBoss on DEV
-    echo   ci:wdbinstall        -- Builds, upgrades JBoss and install database on CI
-    echo   ci                   -- Builds, upgrades JBoss on CI
-    echo   qa:wdbinstall        -- Builds, upgrades JBoss and install database on QA
-    echo   qa                   -- Builds, upgrades JBoss on QA
-    echo   data-qa              -- Builds, upgrades JBoss on DATA-QA
-    echo   deploy               -- Hot deploy application
-    echo   jsp                  -- Hot deploy JSP files
-    echo   stop                 -- Stop war file
-    echo   start                -- Start war file
-    echo   cissh                -- Test SSH login in CI
+    echo   clean        -- Remove classes directory for clean build
+    echo   all          -- Normal build of application
+    echo   upgrade      -- Build and upgrade application
+    echo   install      -- Builds, installs JBoss locally
+    echo   uninstall    -- Uninstall the web application
+    echo   deploy       -- Hot deploy application
+    echo   jsp          -- Hot deploy JSP files
+    echo   config       -- Generates configuration files for a target environment
+    echo   stop         -- Shutdown JBoss
+    echo   start        -- Start JBoss
     goto DONE
 )
-if "%1" == "usage" (
-    ant usage
-    goto DONE
-)
+
 if "%1" == "all" (
     ant %TAG% build:all
     goto DONE
 )
+
 if "%1" == "upgrade" (
     ant %TAG% %DEBUG% deploy:local:upgrade
     goto DONE
 )
+
 if "%1" == "install" (
     ant %TAG% %DEBUG% deploy:local:install
     goto DONE
 )
+
+if "%1" == "uninstall" (
+    ant %TAG% %DEBUG% deploy:local:uninstall
+    goto DONE
+)
+
 if "%1" == "deploy" (
     ant %TAG% %DEBUG% deploy:hot
     goto DONE
 )
-if "%1" == "stop" (
-    ant %TAG% %DEBUG% deploy:stop
-    goto DONE
-)
-if "%1" == "start" (
-    ant %TAG% %DEBUG% deploy:start
-    goto DONE
-)
+
 if "%1" == "jsp" (
     ant %DEBUG% deploy:hot:jsp
     goto DONE
 )
-if "%1" == "upgrade:wdbinstall" (
-    ant -Dupgrade.target=upgrade-ncm:with-dbinstall -Ddatabase.re-create=true %TAG% %DEBUG% deploy:local:upgrade
-    goto DONE
-)
-if "%1" == "dev" (
-    ant -Dproperties.file=%DEVPROPFILE% %TAG% %DEBUG% deploy:remote:upgrade
-    goto DONE
-)
-if not "%1" == "dev:wdbinstall" goto d1
-    @rem *** Remember to set database.drop-schema=true in dev-upgrade.properties file ***
-    type %DEVPROPFILE% | findstr "database.drop-schema=true" >nul
-    if "%errorlevel%" == "0" goto d2
-        echo Error 1:
-        echo   Please set 'database.drop-schema=true' in 'dev-upgrade.properties' file
-        echo   before running this command.
-        echo.
-        goto DONE
-    :d2
-    ant -Dproperties.file=%DEVPROPFILE% -Dupgrade.target=upgrade-ncm:with-dbinstall -Ddatabase.re-create=true %TAG% %DEBUG% deploy:remote:upgrade
-    goto DONE
-:d1
-if "%1" == "ci" (
-    ant -Dproperties.file=%CIPROPFILE% %TAG% %DEBUG% deploy:remote:upgrade
-    goto DONE
-)
-if not "%1" == "ci:wdbinstall" goto c1
-    @rem *** Remember to set database.drop-schema=true in ci-upgrade.properties file ***
-    type %CIPROPFILE% | findstr "database.drop-schema=true" >nul
-    if "%errorlevel%" == "0" goto c2
-        echo Error 1:
-        echo   Please set 'database.drop-schema=true' in 'ci-upgrade.properties' file
-        echo   before running this command.
-        echo.
-        goto DONE
-    :c2
-    ant -Dproperties.file=%CIPROPFILE% -Dupgrade.target=upgrade-ncm:with-dbinstall -Ddatabase.re-create=true %TAG% %DEBUG% deploy:remote:upgrade
-    goto DONE
-:c1
-if "%1" == "qa" (
-    ant -Dproperties.file=%QAPROPFILE% %TAG% %DEBUG% deploy:remote:upgrade
-    goto DONE
-)
-if "%1" == "data-qa" (
-    ant -Dproperties.file=%DATAQAPROPFILE% %TAG% %DEBUG% deploy:remote:upgrade
-    goto DONE
-)
-if not "%1" == "qa:wdbinstall" goto q1
-    @rem *** Remember to set database.drop-schema=true in qa-upgrade.properties file ***
-    type %QAPROPFILE% | findstr "database.drop-schema=true" >nul
-    if "%errorlevel%" == "0" goto q2
-        echo Error 1:
-        echo   Please set 'database.drop-schema=true' in 'qa-upgrade.properties' file
-        echo   before running this command.
-        echo.
-        goto DONE
-    :q2
-    ant -Dproperties.file=%QAPROPFILE% -Dupgrade.target=upgrade-ncm:with-dbinstall -Ddatabase.re-create=true %TAG% %DEBUG% deploy:remote:upgrade
-    goto DONE
-:q1
+
 if "%1" == "clean" (
     ant clean
     if exist ..\target\*.* (
@@ -139,12 +66,20 @@ if "%1" == "clean" (
     goto DONE
 )
 
-if "%1" == "cissh" (
-    ssh jboss51a@ncias-c512-v.nci.nih.gov -i C:\NCI-Projects\ncit-properties\properties\ssh-keys\id_dsa_bda echo "Test worked!"
+if "%1" == "config" (
+    ant %DEBUG% build:config
     goto DONE
 )
-echo.
-echo Unknown command "%1"
-echo.
+
+if "%1" == "stop" (
+    ant %DEBUG% jboss:stop
+    goto DONE
+)
+
+if "%1" == "start" (
+    ant %DEBUG% jboss:start
+    goto DONE
+)
+
 :DONE
 endlocal
